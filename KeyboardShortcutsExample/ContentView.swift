3241ea9a7a739d07dcdbb146614b524a39e874ee
MT <maotong@xiaoyezi.com>
2020-09-30 16:37:28 +0800

    快捷键打开指定app或脚本

--- KeyboardShortcutsExample/ContentView.swift
+++ KeyboardShortcutsExample/ContentView.swift
@@ -9,6 +9,8 @@ extension KeyboardShortcuts.Name {
 struct ContentView: View {
 	@State private var isPressed1 = false
 	@State private var isPressed2 = false
+	@State var softwarePath: String = ""
+	@State var scriptPath: String = ""
 
 	var body: some View {
 		VStack {
@@ -17,12 +19,16 @@ struct ContentView: View {
 					.padding(.trailing, 10)
 				Text("Pressed? \(isPressed1 ? "👍" : "👎")")
 					.frame(width: 100, alignment: .leading)
+				TextField("Enter software path...", text: $softwarePath)
+					.frame(width: 150, alignment: .leading)
 			}
 			HStack {
 				KeyboardShortcuts.Recorder(for: .testShortcut2)
 					.padding(.trailing, 10)
 				Text("Pressed? \(isPressed2 ? "👍" : "👎")")
 					.frame(width: 100, alignment: .leading)
+				TextField("Enter script path...", text: $scriptPath)
+					.frame(width: 150, alignment: .leading)
 			}
 			Spacer()
 			Divider()
@@ -35,6 +41,8 @@ struct ContentView: View {
 			.onAppear {
 				KeyboardShortcuts.onKeyDown(for: .testShortcut1) {
 					isPressed1 = true
+					let appPath = softwarePath
+					NSWorkspace.shared.openFile(appPath) // "/Applications/WeChat.app/"
 				}
 
 				KeyboardShortcuts.onKeyUp(for: .testShortcut1) {
@@ -43,6 +51,10 @@ struct ContentView: View {
 
 				KeyboardShortcuts.onKeyDown(for: .testShortcut2) {
 					isPressed2 = true
+					let task = Process()
+					task.launchPath = scriptPath //"/Users/mt/project/KeyboardShortcuts/makeandroid.sh"
+					task.launch()
+					task.waitUntilExit()
 				}
 
 				KeyboardShortcuts.onKeyUp(for: .testShortcut2) {
